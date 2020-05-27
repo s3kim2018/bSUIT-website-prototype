34643a27347802defa20f6af48caf3519a65f08a
Brian Kim <s3kim2018@berkeley.edu>
2020-05-27 01:00:32 -0700

    added scrolling

--- scripts/comp.js
+++ scripts/comp.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    
+    $('.rapper .head .menu #schedule').on('click', function() {
+        $('html, body, #rapper').animate({
+            scrollTop: $(".rapper .dates").offset().top
+        }, 1000);
+    });
+    $('.rapper .head .menu #about').on('click', function() {
+        $('html, body, #rapper').animate({
+            scrollTop: $(".rapper .about").offset().top
+        }, 1000);
+    });
 })
